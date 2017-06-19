a7c3dc898e17318d5cb58f29d6e78e00cde2f481
Vladyslav Polishchuk <vpolishchuk@amcbridge.com>
2017-06-19 23:05:57 +0300

    Add classes to store base data about participants

---
+++ Core/Model/Participant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class Participant
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class Pair
+    {
+        public Participant Male { get; set; }
+        public Participant Female { get; set; }
+    }
+}
