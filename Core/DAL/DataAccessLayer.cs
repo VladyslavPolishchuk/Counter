4147ad84e889311b46e270b4d4a8a87d4c27d9a5
Vladyslav Polishchuk <vpolishchuk@amcbridge.com>
2017-06-20 22:32:20 +0300

    Add DataAccessLayer class and DbContext implementation

---
+++ Core/DAL/DataAccessLayer.cs
@@ -0,0 +1,16 @@
+﻿using Core.Model;
+
+namespace Core.DAL
+{
+    public class DataAccessLayer
+    {
+        public static void Add(Pair pair)
+        {
+            using (var context = new Core.DAL.Context())
+            {
+                context.Pairs.Add(pair);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Core/DAL/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Model;
+using System.Data.Entity;
+
+namespace Core.DAL
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Context") { }
+        //public DbSet<Participant> Participants { get; set; }
+        public DbSet<Pair> Pairs { get; set; }
+        //public DbSet<Club> Clubs { get; set; }
+        //public DbSet<Judge> Judges { get; set; }
+        //public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
